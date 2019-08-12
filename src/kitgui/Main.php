964a7ff0387194fbc77e014593d22b2acba4c53e
XSlimeX <49905629+XSlimeX@users.noreply.github.com>
2019-08-12 15:26:03 +0300

    Main.php done(for now)

--- src/kitgui/Main.php
+++ src/kitgui/Main.php
@@ -1,1 +1,19 @@
-<
+<?php
+
+namespace kitgui;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use kitgui\KitGUI;
+
+class Main extends PluginBase {
+
+
+    public function onEnable() : void
+   {
+    //kitgui
+    $this->getServer()->getCommandMap()->register("kitgui", new ShopGUI($this));
+    $this->getServer()->getPluginManager()->registerEvents(new ShopGUI($this), $this);
+   }
+ }
